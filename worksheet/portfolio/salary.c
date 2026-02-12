559830a983074d938e36b9f1a6c03f81f0372cb8
sallide <gvzh0213@leeds.ac.uk>
2026-02-12 21:37:11 +0000

    done

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -1,30 +1,40 @@
 
 #include <stdio.h>
+#include <math.h>
 
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name:Saloni Pradhan
+ * ID: 201829493
  */
 
  int main( void ) {
 
     // define and initialise variables for the problem data 
-    float salary = 120000.0;
-    float NI_contribution = 5;
-    int tax_contribution = 15;
+    float salary = 24000.0;
+    float NI_contribution = 8;
+    float tax_contribution = 20;
     float deductions;
     float take_home_salary;
 
+    float NI_percent;
+    float tax_percent;
+
+    float deposit = 120.0;
+    float rate = 0.05;
+    int term = 5;
+    float final_amount;
+    float gain;
+
     // calculate the deductions and final take-home salary
-    NI_contribution = (NI_contribution/100)
-    tax_contribution = (tax_contribution/100)
+    NI_percent = (NI_contribution/100);
+    tax_percent = (tax_contribution/100);
 
-    NI_contribution = salary * NI_contribution
+    NI_contribution = salary * NI_percent;
 
-    tax_contribution = (salary - NI_contribution)*tax_contribution
+    tax_contribution = ((salary - NI_contribution)-12500)*tax_percent;
 
-    take_home_salary = salary //FIX
+    take_home_salary = salary - NI_contribution - tax_contribution;
 
 
 
@@ -35,7 +45,10 @@
     //printf("Tax contribution £...",var_name);
     //printf("Take home salary £...",var_name);
 
-    return 0;
+    printf("Salary £%.2f\n",salary);
+    printf("NI contribution £%.2f\n",NI_contribution);
+    printf("Tax contribution £%.2f\n",tax_contribution);
+    printf("Take home salary £%.2f\n",take_home_salary);
 
 
     /*
