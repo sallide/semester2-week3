b33677f19773e74811f40d63d12d4fd0489d3de5
sallide <gvzh0213@leeds.ac.uk>
2026-02-12 13:48:53 +0000

    FIX

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -10,14 +10,44 @@
  int main( void ) {
 
     // define and initialise variables for the problem data 
+    float salary = 120000.0;
+    float NI_contribution = 5;
+    int tax_contribution = 15;
+    float deductions;
+    float take_home_salary;
 
     // calculate the deductions and final take-home salary
+    NI_contribution = (NI_contribution/100)
+    tax_contribution = (tax_contribution/100)
+
+    NI_contribution = salary * NI_contribution
+
+    tax_contribution = (salary - NI_contribution)*tax_contribution
+
+    take_home_salary = salary //FIX
+
+
+
 
     // Use only these print statement with appropriate formatting and variable names
     //printf("Salary £...",var_name);
     //printf("NI contribution £...",var_name);
     //printf("Tax contribution £...",var_name);
     //printf("Take home salary £...",var_name);
+
+    return 0;
+
+
+    /*
+     * Implement the formula for compound interest.
+     * Print out the final amount and the gain compared to the initial deposit.
+     */
+    
+    final_amount = deposit * pow(( 1 + rate ),term);
+    gain = final_amount - deposit;
+
+    printf("The final amount is %f\n",final_amount);
+    printf("The final gain is %f\n",gain);
 
     return 0;
  }
